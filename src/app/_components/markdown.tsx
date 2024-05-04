1c67942eb2add8646d2107b9987f3d15eac7a5d5
Daniel Lopez <dlopez807@gmail.com>
2024-05-03 21:09:22 -0700

    update order of classes in markdown component

--- src/app/_components/markdown.tsx
+++ src/app/_components/markdown.tsx
@@ -1,7 +1,7 @@
 export default function Markdown({ content }: { content: string }) {
   return (
     <article
-      className='prose lg:prose-xl prose-headings:text-cb-white prose-a:text-cb-pink hover:prose-a:text-cb-pink/75 prose-code:text-cb-mint w-full rounded bg-cb-blue p-4 text-cb-white'
+      className='prose w-full rounded bg-cb-blue p-4 text-cb-white lg:prose-xl prose-headings:text-cb-white prose-a:text-cb-pink hover:prose-a:text-cb-pink/75 prose-code:text-cb-mint'
       dangerouslySetInnerHTML={{ __html: content }}
     />
   )
